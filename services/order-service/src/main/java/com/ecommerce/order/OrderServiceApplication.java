559e4b30319c21bddaa79d1ddafeb03059aff51b
emiledubois <ag.mira@duocuc.cl>
2026-04-04 18:36:15 -0300

    product-service + order-service + auth-service + api-gateway: completed

--- services/order-service/src/main/java/com/ecommerce/order/OrderServiceApplication.java
+++ services/order-service/src/main/java/com/ecommerce/order/OrderServiceApplication.java
@@ -1,4 +1,4 @@
-package com.ecommerce.order.service;
+package com.ecommerce.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- services/product-service/src/main/java/com/ecommerce/product/ProductServiceApplication.java
+++ services/product-service/src/main/java/com/ecommerce/product/ProductServiceApplication.java
@@ -1,4 +1,4 @@
-package com.ecommerce.product.service;
+package com.ecommerce.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
