bcbe6c663a5d7413ce010aa7fb7114ec2d66db8d
emiledubois <ag.mira@duocuc.cl>
2026-04-16 14:43:48 -0400

    refactor (factory): EnvioCreator.java + refactor (inventario): InventarioApplication.java

--- services/ms-envios/src/main/java/com/smartlogix/envios/factory/EnvioCreator.java
+++ services/ms-envios/src/main/java/com/smartlogix/envios/factory/EnvioCreator.java
@@ -1,5 +1,8 @@
 package com.smartlogix.envios.factory;
 
+import com.smartlogix.envios.dto.CreateEnvioRequest;  
+import com.smartlogix.envios.model.Envio;
+
 // Interfaz base del Factory Method
 public interface EnvioCreator {
     Envio crearEnvio(CreateEnvioRequest req);
